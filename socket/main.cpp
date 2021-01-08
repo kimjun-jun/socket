ae7c8f00f85d65f46e9253597033982289041a2b
kim <HP@DESKTOP-5EL7MRK>
2021-01-09 05:23:57 +0900

    実験1

--- socket/main.cpp
+++ socket/main.cpp
@@ -1,200 +1,200 @@
-#define WIN32_LEAN_AND_MEAN
-
-#include <windows.h>
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-
-// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
-#pragma comment (lib, "Ws2_32.lib")
-#pragma comment (lib, "Mswsock.lib")
-#pragma comment (lib, "AdvApi32.lib")
-
-
-#define DEFAULT_BUFLEN 512
-#define DEFAULT_PORT "50000"				//ネットワーク系のポートを指定
-
-
-
-
-int __cdecl main(int argc, char **argv)
-{
-	WSADATA wsaData;
-	SOCKET ConnectSocket = INVALID_SOCKET;
-	struct addrinfo *result = NULL,				//相手のアドレス情報確定
-		*ptr = NULL,							//相手のアドレス情報
-		hints;									//設定用アドレス
-	const char *sendbuf = "this is a test";
-	char recvbuf[DEFAULT_BUFLEN];
-	int iResult;								//エラー全般検出
-	int recvbuflen = DEFAULT_BUFLEN;
-
-	PCSTR ServerADDR;
-	ServerADDR = "192.168.11.100";
-	PCSTR ClientADDR;
-	ClientADDR = "192.168.11.6";
-
-	// Validate the parameters
-	/*
-	if (argc != 2) {
-		printf("usage: %s server-name\n", argv[0]);
-		printf("コマンドプロンプトから実行して引数にサーバーIPアドレスをいれてください\n");
-		printf("失敗!!___enterで終了\n");
-		getchar();
-		return 1;
-	}
-	*/
-
-	// Initialize Winsock
-	//WSAStartup(バージョン指定, WSADATAのアドレス);
-	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-	if (iResult != 0) {
-		switch (iResult) {
-		case WSASYSNOTREADY:
-			printf("WSASYSNOTREADY\n");			//ネットワークサブシステムがネットワークへの接続を準備できていない
-			break;
-		case WSAVERNOTSUPPORTED:
-			printf("WSAVERNOTSUPPORTED\n");		//要求されたwinsockのバージョンがサポートされていない
-			break;
-		case WSAEINPROGRESS:
-			printf("WSAEINPROGRESS\n");			//ブロッキング操作の実行中であるか、 またはサービスプロバイダがコールバック関数を処理している
-			break;
-		case WSAEPROCLIM:
-			printf("WSAEPROCLIM\n");			//winsockが処理できる最大プロセス数に達した
-			break;
-		case WSAEFAULT:
-			printf("WSAEFAULT\n");				//第二引数であるlpWSAData は有効なポインタではない
-			break;
-		}		
-		printf("失敗!!___enterで終了\n");
-		getchar();
-		return 1;
-	}
-
-	ZeroMemory(&hints, sizeof(hints));
-	/*
-	ここが肝
-	hints.ai_family = AF_UNSPEC;
-	AF_UNSPECはunspecified
-	AF_INET→nternetwork: UDP, TCP, etc.
-	*/
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP;
-	hints.ai_flags = AI_PASSIVE;
-
-
-	// Resolve the server address and port
-	//getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result);
-	//第一引数はサーバーのIPアドレスもしくはホスト名
-	//第二引数はサーバーのポートアドレス
-	//iResult = getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result);
-	//サーバー名のIPアドレスを要求するgetaddrinfo関数
-	iResult = getaddrinfo(ServerADDR, DEFAULT_PORT, &hints, &result);
-	if (iResult != 0) {
-		printf("getaddrinfo failed with error: %d\n", iResult);
-		printf("失敗!!___enterで終了\n");
-		getchar();
-		WSACleanup();
-		return 1;
-	}
-
-	// Attempt to connect to an address until one succeeds
-
-	for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
-
-		// Create a SOCKET for connecting to server
-		ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
-			ptr->ai_protocol);
-
-
-		if (ConnectSocket == INVALID_SOCKET) {
-			printf("socket failed with error: %ld\n", WSAGetLastError());
-			printf("失敗!!___enterで終了\n");
-			getchar();
-			WSACleanup();
-			return 1;
-		}
-		// Connect to server.
-		printf("サーバーに接続するよ___enterで接続\n");
-		getchar();
-		iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
-		if (iResult == SOCKET_ERROR) {
-			closesocket(ConnectSocket);
-			ConnectSocket = INVALID_SOCKET;
-			printf("やばい___enterで次へ\n");
-			getchar();
-			continue;
-		}
-		break;
-	}
-
-	// result開放
-	freeaddrinfo(result);
-
-	if (ConnectSocket == INVALID_SOCKET) {
-		printf("Unable to connect to server!\n");
-		printf("失敗!!___enterで終了\n");
-		getchar();
-		WSACleanup();
-		return 1;
-	}
-
-	// Send an initial buffer
-	printf("サーバーに接続成功___enter押してメッセージを送信\n");
-	getchar();
-
-	iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
-	if (iResult == SOCKET_ERROR) {
-		printf("send failed with error: %d\n", WSAGetLastError());
-		closesocket(ConnectSocket);
-		WSACleanup();
-		return 1;
-	}
-	printf("メッセージ送信成功したよ!!\n");
-//	printf("自分のIPアドレス(送信): %s\n",);
-	printf("サーバーからの応答を待ってるよ...\n\n");
-
-	// shutdown the connection since no more data will be sent
-	iResult = shutdown(ConnectSocket, SD_SEND);
-	if (iResult == SOCKET_ERROR) {
-		printf("shutdown failed with error: %d\n", WSAGetLastError());
-		closesocket(ConnectSocket);
-		WSACleanup();
-		return 1;
-	}
-
-	// Receive until the peer closes the connection
-	do {
-
-		iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
-		if (iResult > 0)
-		{
-			printf("サーバーからの応答があったよ!!\n");
-			printf("メッセージが来てるみたい__enter押してメッセージを確認\n");
-			getchar();
-			printf("Bytes received(受信): %d\n", iResult);
-		}
-		else if (iResult == 0)
-		{
-			printf("Connection closed\n");
-		}
-		else
-		{
-			printf("recv failed with error: %d\n", WSAGetLastError());
-			printf("失敗!!___enterで終了\n");
-			getchar();
-		}
-	} while (iResult > 0);
-
-	printf("通信成功!!___enterで終了\n");
-	getchar();
-
-	// cleanup
-	closesocket(ConnectSocket);
-	WSACleanup();
-
-	return 0;
-}
+//#define WIN32_LEAN_AND_MEAN
+//
+//#include <windows.h>
+//#include <winsock2.h>
+//#include <ws2tcpip.h>
+//#include <stdlib.h>
+//#include <stdio.h>
+//
+//
+//// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
+//#pragma comment (lib, "Ws2_32.lib")
+//#pragma comment (lib, "Mswsock.lib")
+//#pragma comment (lib, "AdvApi32.lib")
+//
+//
+//#define DEFAULT_BUFLEN 512
+//#define DEFAULT_PORT "50000"				//ネットワーク系のポートを指定
+//
+//
+//
+//
+//int __cdecl main(int argc, char **argv)
+//{
+//	WSADATA wsaData;
+//	SOCKET ConnectSocket = INVALID_SOCKET;
+//	struct addrinfo *result = NULL,				//相手のアドレス情報確定
+//		*ptr = NULL,							//相手のアドレス情報
+//		hints;									//設定用アドレス
+//	const char *sendbuf = "this is a test";
+//	char recvbuf[DEFAULT_BUFLEN];
+//	int iResult;								//エラー全般検出
+//	int recvbuflen = DEFAULT_BUFLEN;
+//
+//	PCSTR ServerADDR;
+//	ServerADDR = "192.168.11.100";
+//	PCSTR ClientADDR;
+//	ClientADDR = "192.168.11.6";
+//
+//	// Validate the parameters
+//	/*
+//	if (argc != 2) {
+//		printf("usage: %s server-name\n", argv[0]);
+//		printf("コマンドプロンプトから実行して引数にサーバーIPアドレスをいれてください\n");
+//		printf("失敗!!___enterで終了\n");
+//		getchar();
+//		return 1;
+//	}
+//	*/
+//
+//	// Initialize Winsock
+//	//WSAStartup(バージョン指定, WSADATAのアドレス);
+//	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+//	if (iResult != 0) {
+//		switch (iResult) {
+//		case WSASYSNOTREADY:
+//			printf("WSASYSNOTREADY\n");			//ネットワークサブシステムがネットワークへの接続を準備できていない
+//			break;
+//		case WSAVERNOTSUPPORTED:
+//			printf("WSAVERNOTSUPPORTED\n");		//要求されたwinsockのバージョンがサポートされていない
+//			break;
+//		case WSAEINPROGRESS:
+//			printf("WSAEINPROGRESS\n");			//ブロッキング操作の実行中であるか、 またはサービスプロバイダがコールバック関数を処理している
+//			break;
+//		case WSAEPROCLIM:
+//			printf("WSAEPROCLIM\n");			//winsockが処理できる最大プロセス数に達した
+//			break;
+//		case WSAEFAULT:
+//			printf("WSAEFAULT\n");				//第二引数であるlpWSAData は有効なポインタではない
+//			break;
+//		}		
+//		printf("失敗!!___enterで終了\n");
+//		getchar();
+//		return 1;
+//	}
+//
+//	ZeroMemory(&hints, sizeof(hints));
+//	/*
+//	ここが肝
+//	hints.ai_family = AF_UNSPEC;
+//	AF_UNSPECはunspecified
+//	AF_INET→nternetwork: UDP, TCP, etc.
+//	*/
+//	hints.ai_family = AF_INET;
+//	hints.ai_socktype = SOCK_STREAM;
+//	hints.ai_protocol = IPPROTO_TCP;
+//	hints.ai_flags = AI_PASSIVE;
+//
+//
+//	// Resolve the server address and port
+//	//getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result);
+//	//第一引数はサーバーのIPアドレスもしくはホスト名
+//	//第二引数はサーバーのポートアドレス
+//	//iResult = getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result);
+//	//サーバー名のIPアドレスを要求するgetaddrinfo関数
+//	iResult = getaddrinfo(ServerADDR, DEFAULT_PORT, &hints, &result);
+//	if (iResult != 0) {
+//		printf("getaddrinfo failed with error: %d\n", iResult);
+//		printf("失敗!!___enterで終了\n");
+//		getchar();
+//		WSACleanup();
+//		return 1;
+//	}
+//
+//	// Attempt to connect to an address until one succeeds
+//
+//	for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+//
+//		// Create a SOCKET for connecting to server
+//		ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
+//			ptr->ai_protocol);
+//
+//
+//		if (ConnectSocket == INVALID_SOCKET) {
+//			printf("socket failed with error: %ld\n", WSAGetLastError());
+//			printf("失敗!!___enterで終了\n");
+//			getchar();
+//			WSACleanup();
+//			return 1;
+//		}
+//		// Connect to server.
+//		printf("サーバーに接続するよ___enterで接続\n");
+//		getchar();
+//		iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+//		if (iResult == SOCKET_ERROR) {
+//			closesocket(ConnectSocket);
+//			ConnectSocket = INVALID_SOCKET;
+//			printf("やばい___enterで次へ\n");
+//			getchar();
+//			continue;
+//		}
+//		break;
+//	}
+//
+//	// result開放
+//	freeaddrinfo(result);
+//
+//	if (ConnectSocket == INVALID_SOCKET) {
+//		printf("Unable to connect to server!\n");
+//		printf("失敗!!___enterで終了\n");
+//		getchar();
+//		WSACleanup();
+//		return 1;
+//	}
+//
+//	// Send an initial buffer
+//	printf("サーバーに接続成功___enter押してメッセージを送信\n");
+//	getchar();
+//
+//	iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
+//	if (iResult == SOCKET_ERROR) {
+//		printf("send failed with error: %d\n", WSAGetLastError());
+//		closesocket(ConnectSocket);
+//		WSACleanup();
+//		return 1;
+//	}
+//	printf("メッセージ送信成功したよ!!\n");
+////	printf("自分のIPアドレス(送信): %s\n",);
+//	printf("サーバーからの応答を待ってるよ...\n\n");
+//
+//	// shutdown the connection since no more data will be sent
+//	iResult = shutdown(ConnectSocket, SD_SEND);
+//	if (iResult == SOCKET_ERROR) {
+//		printf("shutdown failed with error: %d\n", WSAGetLastError());
+//		closesocket(ConnectSocket);
+//		WSACleanup();
+//		return 1;
+//	}
+//
+//	// Receive until the peer closes the connection
+//	do {
+//
+//		iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+//		if (iResult > 0)
+//		{
+//			printf("サーバーからの応答があったよ!!\n");
+//			printf("メッセージが来てるみたい__enter押してメッセージを確認\n");
+//			getchar();
+//			printf("Bytes received(受信): %d\n", iResult);
+//		}
+//		else if (iResult == 0)
+//		{
+//			printf("Connection closed\n");
+//		}
+//		else
+//		{
+//			printf("recv failed with error: %d\n", WSAGetLastError());
+//			printf("失敗!!___enterで終了\n");
+//			getchar();
+//		}
+//	} while (iResult > 0);
+//
+//	printf("通信成功!!___enterで終了\n");
+//	getchar();
+//
+//	// cleanup
+//	closesocket(ConnectSocket);
+//	WSACleanup();
+//
+//	return 0;
+//}
--- socket/winsock2-0clientTCP.cpp
+++ socket/winsock2-0clientTCP.cpp
@@ -1,60 +1,165 @@
-//#include <stdio.h>
-//#include <winsock2.h>
-//#include <ws2tcpip.h>
-//#pragma comment (lib, "Ws2_32.lib")
-//
-////STEP1	同端末通信	OK
-////STEP2	LAN通信		
-////STEP3	WAN通信	
-//
-//int main() 
-//{
-//	/* IP アドレス、ポート番号、ソケット */
-//	char destination[80];
-//	unsigned short port = 27015;
-//	int dstSocket;
-//
-//	/* sockaddr_in 構造体 */
-//	struct sockaddr_in dstAddr;
-//
-//	/* 各種パラメータ */
-//	//int status;
-//	//int numsnt;
-//	char toSendText[1024] = "This is a test";
-//
-//	/************************************************************/
-//
-//	/* Windows 独自の設定 */
-//	WSADATA data;
-//	WSAStartup(MAKEWORD(2, 0), &data);
-//
-//	/* 相手先アドレスの入力 */
-//	printf("Connect to ? : (name or IP address) ");
-//	gets_s(destination);
-//
-//	/* sockaddr_in 構造体のセット */
-//	memset(&dstAddr, 0, sizeof(dstAddr));
-//	inet_pton(AF_INET,destination, &dstAddr.sin_addr);
-//	dstAddr.sin_port = htons(port);
-//	dstAddr.sin_family = AF_INET;
-//
-//	/* ソケット生成 */
-//	dstSocket = socket(AF_INET, SOCK_STREAM, 0);
-//
-//	/* 接続 */
-//	printf("Trying to connect to %s: \n", destination);
-//	connect(dstSocket, (struct sockaddr *) &dstAddr, sizeof(dstAddr));
-//
-//	/* パケット送出 */
-//	for (int i = 0; i < 10; i++) {
-//		printf("sending...\n");
-//		send(dstSocket, toSendText, strlen(toSendText) + 1, 0);
-//		Sleep(1000);
-//	}
-//
-//	/* Windows 独自の設定 */
-//	closesocket(dstSocket);
-//	WSACleanup();
-//
-//	return 0;
-//}
+#include <stdio.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <string.h>
+#include <thread>
+
+#pragma comment (lib, "Ws2_32.lib")
+#define BUFFER_SIZE 100
+
+//STEP1	同端末通信	OK
+//STEP2	LAN通信		
+//STEP3	WAN通信	
+
+using namespace std;
+
+
+//////////////////////////////////////////////////////////
+
+int NetClientSocketCreate();
+void Packet();
+void SendPacket(int i);
+void ReceivePacket();
+
+//////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////
+/* IP アドレス、ポート番号、ソケット */
+char destination[80];
+unsigned short port = 27015;
+int dstSocket;
+
+/* sockaddr_in 構造体 */
+struct sockaddr_in dstAddr;
+
+/* 各種パラメータ */
+int numrcv;
+char buffer[BUFFER_SIZE]; //送られてくるデータ内容
+char toSendText[1024] = "1";
+int status;
+//////////////////////////////////////////////////////////
+
+
+int main() 
+{
+
+	NetClientSocketCreate();
+		
+	//マルチスレッドで受信プログラム(受信関数)起動　細かく通信する　1/60じゃない　関数一つで出来る
+	//マルチスレッド開始
+	std::thread t1(Packet);
+
+	while (1)
+	{
+
+	}
+
+	/* Windows 独自の設定 */
+	closesocket(dstSocket);
+	WSACleanup();
+
+	return 0;
+}
+
+int NetClientSocketCreate(void)
+{
+	/* Windows 独自の設定 */
+	WSADATA data;
+	WSAStartup(MAKEWORD(2, 0), &data);
+
+	/* 相手先アドレスの入力 */
+	//printf("Connect to ? : (name or IP address) ");
+	//gets_s(destination);
+
+	sprintf_s(destination, "192.168.11.3");
+
+	/* sockaddr_in 構造体のセット */
+	memset(&dstAddr, 0, sizeof(dstAddr));
+	inet_pton(AF_INET, destination, &dstAddr.sin_addr);
+	dstAddr.sin_port = htons(port);
+	dstAddr.sin_family = AF_INET;
+
+	/* ソケット生成 */
+	dstSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	/* 接続 */
+	printf("Trying to connect to %s: \n", destination);
+	connect(dstSocket, (struct sockaddr *) &dstAddr, sizeof(dstAddr));
+
+	// ここで、ノンブロッキングに設定しています。
+	 // val = 0でブロッキングモードに設定できます。
+	 // ソケットの初期設定はブロッキングモードです。
+	u_long val = 1;
+	ioctlsocket(dstSocket, FIONBIO, &val);
+
+	return 0;
+}
+
+void Packet(void)
+{
+	/*
+	while (1)
+	{
+		SendPacket();
+		ReceivePacket();
+	}
+	*/
+
+	for(int i=0;i<10;i++)
+	{
+		SendPacket(i);
+		ReceivePacket();
+	}
+
+}
+
+
+void SendPacket(int i)
+{
+	/* パケット送出 */
+	printf("Send回数%d\n",i+1);
+	send(dstSocket, toSendText, strlen(toSendText) + 1, 0);
+	numrcv = recv(dstSocket, buffer, BUFFER_SIZE, 0);
+	if (numrcv < 1)
+	{
+		if (WSAGetLastError() == WSAEWOULDBLOCK)
+		{
+			// まだ来ない。
+			//printf("ただ今の接続人数%s\n", buffer);
+		}
+		else
+		{
+			printf("error : 0x%x\n", WSAGetLastError());
+		}
+	}
+	else
+	{
+		printf("received: ただ今の接続人数%s\n", buffer);
+	}
+	Sleep(1000);
+}
+
+void ReceivePacket(void)
+{
+	char RMsg[BUFFER_SIZE] = { NULL }; //送られてくるデータ内容
+
+	//常時受け取りをする。データの変更があった時のみアクセス許可を取り、対象データに書き込みを行う。
+	numrcv = recv(dstSocket, RMsg, BUFFER_SIZE, 0);
+	if (numrcv < 1)
+	{
+		if (WSAGetLastError() == WSAEWOULDBLOCK)
+		{
+			// まだ来ない。
+			//printf("ただ今の接続人数%s\n", buffer);
+		}
+		else
+		{
+			//printf("error : 0x%x\n", WSAGetLastError());
+		}
+	}
+	else
+	{
+			printf("Receive %s\n", RMsg);
+	}
+}
